e6ae3c1b761d3ec830957d1021b4e35b4769dd33
Poojabhagwat498 <poojabhagwat2525@gmail.com>
2026-04-23 23:06:11 +0530

    update bakcend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/workouts", require("./routes/workouts"));
+
+app.listen(5000, ()=>console.log("Server running on port 5000"));
